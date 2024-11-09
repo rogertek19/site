623e17cc20b8b4c50fcc2667b27f66a372c2684c
Roger Festino <126997843+rogertek19@users.noreply.github.com>
2024-11-08 22:13:10 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var dow1 = window.document.querySelector('#baixar1')
+var dow2 = window.document.querySelector('#baixar2')
+dow1.addEventListener('click', baixar1)
+dow2.addEventListener('click', baixar2)
 function baixar1() {
   alert('O download começará em breve');
 }
